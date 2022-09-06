9b6f90529cd241d8a37ab9e525f865d1eda664cd
Marc Bourgoin <themard85@gmail.com>
2022-09-06 07:31:13 -0600

    msm8998-common: Drop liboemcrypto blob
    Unfortunately even though both berlin and dubai show Widevine L1 status?
    It doesn't work. Apps like Netflix fail to load.
    
    logcat shows errors such as:
    E WVCdm   : [device_files.cpp(1111):RetrieveHashedFile] File does not exist: path = /data/vendor/mediadrm/IDM1013/L1/cert.bin
    W WVCdm   : [device_files.cpp(144):RetrieveCertificate] Unable to retrieve certificate file
    
    So DRM keys probably got wiped upon bootloader unlock as even stock ROM
    this same behavior happens.
    
    Change-Id: Ifae38b82499b1470f8c83bf2caf1848961ef9b8e
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- msm8998-common-vendor.mk
+++ msm8998-common-vendor.mk
@@ -477,7 +477,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/msm8998-common/proprietary/vendor/lib64/libnetmgr_nr_fusion.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libnetmgr_nr_fusion.so \
     vendor/xiaomi/msm8998-common/proprietary/vendor/lib64/libnetmgr_rmnet_ext.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libnetmgr_rmnet_ext.so \
     vendor/xiaomi/msm8998-common/proprietary/vendor/lib64/libnlnetmgr.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libnlnetmgr.so \
-    vendor/xiaomi/msm8998-common/proprietary/vendor/lib64/liboemcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liboemcrypto.so \
     vendor/xiaomi/msm8998-common/proprietary/vendor/lib64/libops.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libops.so \
     vendor/xiaomi/msm8998-common/proprietary/vendor/lib64/libpdmapper.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libpdmapper.so \
     vendor/xiaomi/msm8998-common/proprietary/vendor/lib64/libpdnotifier.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libpdnotifier.so \
